98c4af7135e1c73f5e2c979721585ea95e9abb5f
mav-21 <alexyounis244@gmail.com>
2025-11-20 23:56:50 -0500

    commit-5: actually fixed the error i hope

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // --- imports
+require('dotenv').config();
 const express = require('express');                   // main web framework
 const path = require('path');                        // helps with directory paths
 const expressLayouts = require('express-ejs-layouts'); // for using layout.ejs
 const methodOverride = require('method-override');   // needed to use PUT/DELETE
-const dotenv = require('dotenv');                    // loads .env file
-dotenv.config();                                     // load the values
+
 
 const connectDB = require('./config/db');            // our db connection file
 
-// connect to mongo first
+// connect to mongo 
 connectDB();
 
 const app = express();                               // creating express app
